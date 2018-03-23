a587da164314206b009770d0ba824cdb02e85285
Bryan Grill <grillb@gmail.com>
2018-03-23 04:11:23 -0400

    start add esri loader

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,3 +1,4 @@
+import { loadCss, loadModules } from 'esri-loader';
 import * as React from 'react';
 
 export interface IHelloProps {
@@ -5,10 +6,32 @@ export interface IHelloProps {
   framework: string;
 }
 
-export const Hello = (props: IHelloProps) => {
-  return (
-    <div>
-      Hello from {props.compiler} and {props.framework}!
-    </div>
-  );
-};
+export class Hello extends React.Component<IHelloProps, {}> {
+  public componentDidMount() {
+    loadCss('https://js.arcgis.com/4.6/esri/css/main.css');
+    loadModules(['esri/views/MapView', 'esri/WebMap'])
+      .then(([MapView, WebMap]) => {
+        const map = new WebMap({
+          portalItem: {
+            id: 'f2e9b762544945f390ca4ac3671cfa72'
+          }
+        });
+        const view = new MapView({
+          map,
+          container: this.map,
+        });
+      })
+      .catch((e: Error) => {
+        console.log(e);
+      });
+  }
+  public render() {
+    return (
+      <div>
+        <div className="map">
+          <div ref={(map) => { this.map = map; }} />
+        </div>
+      </div>
+    );
+  }
+}
