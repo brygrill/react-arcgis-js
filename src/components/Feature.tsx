cbdeb97187335dba271d0a64ab3a04fa6b989747
Bryan Grill <grillb@gmail.com>
2018-03-26 16:57:06 -0400

    start add feature component

---
+++ src/components/Feature.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+
+export interface IFeatureProps {
+  map: object;
+  view: object;
+}
+
+export interface IFeatureState {
+  feature: object;
+}
+
+export class Feature extends React.Component<IFeatureProps, IFeatureState> {
+
+  constructor(props: IFeatureProps) {
+    super(props);
+    this.state = {
+      feature: {},
+    };
+  }
+
+  componentDidMount() {
+    console.log(this.props);
+  }
+
+  render() {
+    return <div/>
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { ILoadScriptOptions, loadModules } from 'esri-loader';
+import * as FeatureComponent from './components/Feature';
 import * as MapComponent from './components/Map';
 
 const reactArcGisJS = {
   Map: MapComponent.Map,
+  Feature: FeatureComponent.Feature,
   esriLoader: loadModules,
 };
 
 export default reactArcGisJS;
 
 export const Map = reactArcGisJS.Map;
+export const Feature = reactArcGisJS.Feature;
