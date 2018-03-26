3f80b823399528946885a391f8feb0a65a5393fb
Bryan Grill <grillb@gmail.com>
2018-03-26 16:45:02 -0400

    add basemap enum

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -3,6 +3,17 @@ export enum BaseMapOptions {
   satellite = 'satellite',
   hybrid = 'hybrid',
   topo = 'topo',
+  gray = 'gray',
+  dark_gray = 'dark-gray',
+  oceans = 'oceans',
+  national_geographic = 'national-geographic',
+  terrain = 'terrain',
+  osm = 'osm',
+  dark_gray_vector = 'dark-gray-vector',
+  gray_vector = 'gray-vector',
+  streets_vector = 'streets-vector',
+  topo_vector = 'topo-vector',
+  streets_night_vector = 'streets-night-vector',
   streets_relief_vector = 'streets-relief-vector',
   streets_navigation_vector = 'streets-navigation-vector'
 }
