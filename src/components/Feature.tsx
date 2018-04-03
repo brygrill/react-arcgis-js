3744b6a1be4531876bb2adbd88b1d7c6b3bc0e7a
Bryan Grill <grillb@gmail.com>
2018-04-03 12:17:47 -0400

    update to render FL via url prop

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -2,29 +2,48 @@ import * as React from 'react';
 
 import { loadFeatureLayerModule } from '../helpers';
 
+export interface IMapPropInterface {
+  add: Function;
+}
 export interface IFeatureProps {
-  map: object;
+  map: IMapPropInterface;
   view: object;
+  url: string;
 }
 
 export interface IFeatureState {
+  error: boolean;
   feature: object;
 }
 
 export class Feature extends React.Component<IFeatureProps, IFeatureState> {
-
   constructor(props: IFeatureProps) {
     super(props);
+    this.createFeature = this.createFeature.bind(this);
     this.state = {
+      error: false,
       feature: {},
     };
   }
 
+  async createFeature() {
+    try {
+      const FeatureLayer = await loadFeatureLayerModule();
+      const feature = new FeatureLayer({
+        url: this.props.url,
+      });
+      this.props.map.add(feature);
+      this.setState({ feature });
+    } catch (error) {
+      this.setState({ error: true });
+    }
+  }
+
   componentDidMount() {
-    console.log(this.props);
+    this.createFeature();
   }
 
   render() {
-    return <div/>
+    return <div />;
   }
 }
