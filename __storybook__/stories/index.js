664e02461bb48da9fd6e95698d0504d8630bad5e
Bryan Grill <grillb@gmail.com>
2018-04-03 12:18:03 -0400

    add story of map and FL

--- __storybook__/stories/index.js
+++ __storybook__/stories/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { Map } from '../../dist';
-
-const Child = props => {
-  console.log(props);
-  return (
-    <div>
-      MyChild
-    </div>
-  );
-};
+import { Map, Feature } from '../../dist';
 
 storiesOf('Map', module)
+  .add('with item id', () => <Map itemId="aea0b8bf28884a27bfb5523b3d6d6aeb" />)
   .add('with no item id', () => (
-    <Map webGL><Child/></Map>
+    <Map webGL/>
   ))
-  .add('with item id', () => <Map itemId="aea0b8bf28884a27bfb5523b3d6d6aeb" />);
+  .add('with feature layer url', () => (
+    <Map webGL>
+      <Feature url="https://sampleserver6.arcgisonline.com/arcgis/rest/services/Census/MapServer/3"/>
+    </Map>
+  ));
