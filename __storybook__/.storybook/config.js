34f8eeb9f056cc06a4e970e979fcc8294302da72
Bryan Grill <grillb@gmail.com>
2018-04-05 16:18:24 -0400

    add react markdown

--- __storybook__/.storybook/config.js
+++ __storybook__/.storybook/config.js
@@ -1,11 +1,5 @@
 import { configure } from '@storybook/react';
-import { setDefaults } from '@storybook/addon-info';
 import '../styles/index.css';
-
-setDefaults({
-  inline: true,
-  source: false,
-});
 
 function loadStories() {
   require('../stories/index.js');
--- __storybook__/stories/index.js
+++ __storybook__/stories/index.js
@@ -1,28 +1,33 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { withInfo } from '@storybook/addon-info';
+import ReactMarkdown from 'react-markdown';
 import { Map, Feature } from '../../dist';
 
 const resp = resp => {
   console.log(resp);
 };
 
+const content =
+`
+\`\`\`jsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Map } from 'react-arcgis-js';
+
+ReactDOM.render(
+  <Map />,
+  document.getElementById('root')
+);
+\`\`\`
+`
+
 storiesOf('Map', module)
-
-  .add(
-    'Props',
-    withInfo(`
-  description or documentation about my component, supports markdown
-
-  ~~~js
-  <Button>Click Here</Button>
-  ~~~
-
-  `)(() => <Map />),
-  )
-  .add('Example - with itemId prop', () => (
-    <Map itemId="aea0b8bf28884a27bfb5523b3d6d6aeb" />
+  .add('Props', () => (
+    <ReactMarkdown source={content} />
+  ))
+  .add('Example - with no props', () => (
+    <Map />
   ))
   .add('With itemId prop', () => (
     <Map itemId="aea0b8bf28884a27bfb5523b3d6d6aeb" />
